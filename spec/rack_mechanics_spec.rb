2a5088deea52febebf2124f8e864399c8a103c21
github username <github email address>
2018-08-05 13:00:28 +0100

    Done.

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper.rb'
-
+require 'Rack'
 describe "Application" do
   it "should introduce itself" do
     get "/"
     expect(last_response.body).to include ("Hello, my name is")
+  end
+
+  def call(env)
+    resp = Rack::Response.new
+    resp.write "Hello, World"
+    resp.finish
   end
 end
